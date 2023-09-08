f4b7319fc567f3b203d8bc1344435a1dde65ca62
sifat <sifat0666@gmail.com>
2023-09-09 00:45:33 +0600

    bug fix

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,8 @@ const state = proxy({
   isLogoTexture: true,
   logoDecal: "./threejs.png",
   fullDecal: "./threejs.png",
-  changePosition,
-  setChangePosition,
+  changePosition: "",
+  setChangePosition: "",
 });
 
 export default state;
